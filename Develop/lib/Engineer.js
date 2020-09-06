3b89ceba31e45e2f54bacd2ba07d6af0df9cc132
Angel Alexander <angale22@gmail.com>
2020-09-06 15:42:31 -0500

    added Intern class with properties and methods

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -2,13 +2,13 @@
 const Employee = require("./Employee");
 
 class Engineer extends Employee {
-    constructor(name, id, email, github){
+    constructor(name, id, email, github) {
         super(name, id, email);
         this.github = github;
     }
 
     getGithub() {
-        return this.github
+        return this.github;
     }
 
     getRole() {
